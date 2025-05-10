502986a8e3c473049a25ece39a432b34e5714487
suvarnalakshmi123 <bottaksl2003@gmail.com>
2025-05-10 23:39:24 +0530

    Modify process to fetch today's supplier, sales, product, and customer data from GCS buckets via API

--- server/key.py
+++ server/key.py
@@ -7,10 +7,3 @@ key_path = r"D:\ETL_pipeline_project\server\meta-morph-d-eng-pro-view-key.json"
 # Initialize a GCS client
 client = storage.Client.from_service_account_json(key_path)
 
-# Define bucket name and today's date
-bucket_name = "meta-morph"
-today_date = datetime.today().strftime('%Y-%m-%d') 
-blob_name = f"{today_date}/supplier_{today_date}.csv"
-
-# Get the bucket
-bucket = client.get_bucket(bucket_name)
